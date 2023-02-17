712f608181429349ab01b154ac3874f044e87164
SecondAttempt-GH <SecondAttempt-GH@gmail.com>
2023-02-17 14:43:32 +0300

    Перенастрокйка

--- server/main.py
+++ server/main.py
@@ -9,8 +9,7 @@ from app.database.databasehander import database_handler
 def main() -> None:
     loop = asyncio.new_event_loop()
     asyncio.run_coroutine_threadsafe(database_handler.run_process_requests(), loop)
-    print("Port changed")
-    config = Config(app=app, loop=loop, host="0.0.0.0", port=8000)
+    config = Config(app=app, loop=loop, host="0.0.0.0", port=80)
     server = Server(config)
     loop.run_until_complete(server.serve())
     loop.close()
